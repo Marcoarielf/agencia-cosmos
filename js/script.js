3ef442b8df45032b6a77d24b6f39d65390c40e89
Marcoarielf <64933125+Marcoarielf@users.noreply.github.com>
2021-07-06 13:19:36 -0300

    first commit

---
+++ js/script.js
@@ -0,0 +1,29 @@
+'use strict';
+
+//open modal
+var modales = $(".myBtn");
+var closeModal = $('.close');
+
+function handlerClickModal() {
+    $('body').addClass('no-scroll');
+    const idModal = $(this).attr("id");
+    const modal = $('.'+idModal);
+    modal.css("z-index","5000");
+    modal.css("background" , "rgba(0,0,0,0.6)");
+    const tlModal = gsap.timeline();
+    tlModal.to(modal, .5, { opacity: 1})
+    .to(".modal-content", .5, {opacity: 1, y:45, ease: Power1.easeOut },'-=.5')
+    .to(".modal-content h3", .5, {opacity: 1, ease: Power1.easeOut },'-=.5')
+    .to(".modal-content p", .5, {opacity: 1, y:20, ease: Power1.easeOut },'-=.5');
+   
+   closeModal.on("click",function() {
+       $('body').removeClass('no-scroll');
+       modal.css("z-index","-1");
+       TweenMax.to(modal, 0.2, { opacity: 0});
+       TweenMax.to(".modal-content", 0.5, {ease: Power1.easeOut });
+    });
+    
+}
+for (var i = 0 ; i < modales.length; i++) {
+  modales[i].addEventListener('click' , handlerClickModal , false ) ; 
+}
