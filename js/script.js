dcdce2b7973c07e850f13b23d566f4218ab896b5
marco.filetto <marcoarielf96@gmail.com>
2021-07-07 15:44:40 -0300

    updates

--- js/script.js
+++ js/script.js
@@ -12,10 +12,19 @@ function handlerClickModal() {
     modal.css("background" , "rgba(0,0,0,0.6)");
     const tlModal = gsap.timeline();
     tlModal.to(modal, .5, { opacity: 1})
-    .to(".modal-content", .5, {opacity: 1, y:45, ease: Power1.easeOut },'-=.5')
+    .to(".modal-content", .5, {opacity: 1, y:0, ease: Power1.easeOut },'-=.5')
     .to(".modal-content h3", .5, {opacity: 1, ease: Power1.easeOut },'-=.5')
     .to(".modal-content p", .5, {opacity: 1, y:20, ease: Power1.easeOut },'-=.5');
    
+    $(document).keydown(function(event) { 
+      if (event.keyCode == 27) { 
+        $('body').removeClass('no-scroll');
+       modal.css("z-index","-1");
+       TweenMax.to(modal, 0.2, { opacity: 0});
+       TweenMax.to(".modal-content", 0.5, {ease: Power1.easeOut });
+      }
+    });
+
    closeModal.on("click",function() {
        $('body').removeClass('no-scroll');
        modal.css("z-index","-1");
